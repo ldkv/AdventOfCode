9175ac920393715cdf0f55cea9783dfaaa023971
Vu LE <ledkvu@gmail.com>
2022-12-03 00:24:42 +0100

    Update tools to take into account test input file

--- 2022/aoc_tools.py
+++ 2022/aoc_tools.py
@@ -1,4 +1,4 @@
-# Tools for AoC 2022
+# Tools for AoC 2021
 import os
 
 def clean_line(line, convert_to_int = True):
@@ -8,8 +8,11 @@ def clean_line(line, convert_to_int = True):
     return l
 
 
-def generate_input_filename(day):
-    return f"./inputs/input_{day}.txt"
+def generate_input_filename(day, test_file=False):
+    test = ""
+    if test_file:
+        test = "test_"
+    return f"./inputs/{test}input_{day}.txt"
 
 
 def get_inputs(input_file, convert_to_int=False):
@@ -21,6 +24,6 @@ def get_inputs(input_file, convert_to_int=False):
     return inputs
 
 
-def generate_input_filename_and_get_inputs(day, convert_to_int=False):
-    input_filename = generate_input_filename(day)
+def generate_input_filename_and_get_inputs(day, convert_to_int=False, test_file=False):
+    input_filename = generate_input_filename(day, test_file)
     return get_inputs(input_filename, convert_to_int)
