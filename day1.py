086e422797e49eb4c5d2aa11f9fe3a8275bf6b48
Vu LE <ledkvu@gmail.com>
2022-12-02 14:51:45 +0100

    reformat day 1

--- day1.py
+++ day1.py
@@ -1,19 +1,18 @@
-import bisect
-
 day = 1
 
 def solution(f):
-    sorted = []
+    elves = []
     add = 0
     for line in f:
         if line == '\n':
-            bisect.insort(sorted, add)
+            elves.append(add)
             add = 0
             continue
         add += int(line)
     
-    print(sorted[-1])
-    print(sum(sorted[-3:]))
+    elves.sort() # NlogN
+    print(elves[-1])
+    print(sum(elves[-3:]))
 
 if __name__ == '__main__':
     input_file = f"./inputs/input_day{day}.txt"
