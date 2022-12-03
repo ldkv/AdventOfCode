dd0d009a86312ae2bbccf60f9f6730d91bec30d4
Vu LE <ledkvu@gmail.com>
2022-12-03 08:20:38 +0100

    Solution for day 3 of AoC2022

---
+++ 2022/day3.py
@@ -0,0 +1,44 @@
+import os
+import aoc_tools
+
+def calc_priority(letter):
+    if ord(letter) >= 97:
+        return ord(letter) - ord('a') + 1
+    else:
+        return ord(letter) - ord('A') + 27
+
+
+def solution_part1(inputs):
+    res = 0
+    for line in inputs:
+        ln = len(line)
+        if ln % 2 == 1:
+            print(ln, line)
+        for let in line[:ln//2]:
+            if let in line[ln//2:]:
+                res += calc_priority(let)
+                break
+    print(res)
+    
+
+def solution_part2(inputs):
+    res = 0
+    idx = 0
+    while idx < len(inputs):
+        first = ''
+        for let in inputs[idx]:
+            if let in inputs[idx + 1]:
+                first += let
+        for let in first:
+            if let in inputs[idx + 2]:
+                res += calc_priority(let)
+                break
+        idx += 3
+    print(res)
+
+    
+if __name__ == '__main__':
+    day = os.path.basename(__file__).split('.')[0]
+    inputs = aoc_tools.generate_input_filename_and_get_inputs(day, test_file=False)
+    solution_part1(inputs)
+    solution_part2(inputs)
