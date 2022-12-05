f14ee83413be5e44d9af8893555f5b7b51e67cc0
Vu LE <ledkvu@gmail.com>
2022-12-05 21:35:03 +0100

    Updated solution for day 5 with a better general parser

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,23 +1,30 @@
 import os
-from collections import deque 
 import aoc_tools
 
 def parse_inputs(inputs):
-    stacks = [deque() for _ in range((len(inputs[0]) + 1) // 4)]
-    i = 0
-    while inputs[i][1] != '1':
-        line = inputs[i]
-        for idx in range(1, len(line), 4):
-            if line[idx].strip():
-                stacks[idx // 4].appendleft(line[idx])
-        i += 1
+    # Find line with number order of the stacks
+    stack_line = 0
+    while not (inputs[stack_line][0]==' ' and inputs[stack_line][1]=='1'):
+        stack_line += 1
     
+    # Calculate space (number of characters) between each cargo then parse stacks
+    nb_stacks = int(inputs[stack_line].split(' ')[-1])
+    stacks = [[] for _ in range(nb_stacks)]
+    spaces = len(inputs[stack_line]) // nb_stacks
+    for i in range(stack_line):
+        line = inputs[i]
+        for idx in range(1, len(line), spaces):
+            if line[idx].strip():
+                stacks[idx // spaces].append(line[idx])
+    reverse_stacks = [s[::-1] for s in stacks]
+
+    # Parse commands
     commands = []
-    for line in inputs[i+2:]:
+    for line in inputs[stack_line+2:]:
         line = line.replace('move ', '').replace('from ', '').replace('to ', '')
         commands.append([int(x) for x in line.split(' ')])
-    
-    return (stacks, commands)
+
+    return (reverse_stacks, commands)
 
 
 def solution_part1(inputs):
@@ -57,7 +64,7 @@ if __name__ == '__main__':
     inputs = aoc_tools.generate_input_filename_and_get_inputs(
         day,
         raw_input=True,
-        test_file=False
+        test_file=True
     )
     print(f"Solution for {day} part 1 = {solution_part1(inputs)}")
     print(f"Solution for {day} part 2 = {solution_part2(inputs)}")
