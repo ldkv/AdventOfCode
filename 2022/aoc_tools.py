c214baf5cb7c9a69a9e66da6807097deb1b442bc
Vu LE <ledkvu@gmail.com>
2022-12-07 23:29:55 +0100

    add timeit decorator to measure execution time

--- 2022/aoc_tools.py
+++ 2022/aoc_tools.py
@@ -1,5 +1,19 @@
 # Tools for AoC 2022
 import os
+import time
+from functools import wraps
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__} took {total_time*1000:.2f} ms')
+        return result
+    return timeit_wrapper
+
 
 def clean_line(line, convert_to_int=True):
     l = line.strip()
