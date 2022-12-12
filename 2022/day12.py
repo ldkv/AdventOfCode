3d926b3dc9b4781fec682ca03d6acae6ae3eeab7
Vu LE <ledkvu@gmail.com>
2022-12-12 11:31:34 +0100

    Day 12 solution refactored.

--- 2022/day12.py
+++ 2022/day12.py
@@ -8,31 +8,16 @@ def find_start_end(grid):
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 'S':
-                start = (x, y)#, 'a', 0)
+                start = (x, y)
                 if end:
                     return(start, end)
                 continue
             if grid[x][y] == 'E':
-                end = (x, y)#, 'z', -1)
+                end = (x, y)
                 if start:
                     return(start, end)
                 continue
     return start, end
-
-
-def find_neighbors(pos, grid):
-    (x, y, h, depth) = pos
-    height = ord(h)
-    neighbors = []
-    if x > 0 and abs(ord(grid[x-1][y]) - height) <= 1:
-        neighbors.append((x-1, y, grid[x-1][y], depth+1))
-    if x < len(grid)-1 and abs(ord(grid[x+1][y]) - height) <= 1:
-        neighbors.append((x+1, y, grid[x+1][y], depth+1))
-    if y > 0 and abs(ord(grid[x][y-1]) - height) <= 1:
-        neighbors.append((x, y-1, grid[x][y-1], depth+1))
-    if y < len(grid[0])-1 and abs(ord(grid[x][y+1]) - height) <= 1:
-        neighbors.append((x, y+1, grid[x][y+1], depth+1))
-    return neighbors
 
 
 def height(letter):
@@ -43,65 +28,39 @@ def height(letter):
     return ord(letter)
 
 
-def solution_part1(grid):
+@aoc_tools.timeit
+def find_shortest_path(grid: list, start: tuple, ends: set, go_up=True):
     H = len(grid)
     W = len(grid[0])
-    start, end = find_start_end(grid)
-    visited = set([start])
+    visited = set(start)
     queue = deque([[start]])
     while queue:
         path = queue.popleft()
         x, y = path[-1]
-        if grid[x][y] == 'E':
+        # Stop condition
+        if grid[x][y] in ends:
             return len(path) - 1
+
         curr_height = height(grid[x][y])
+        # Check valid neighbors
         for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
             if 0 <= x2 < H and 0 <= y2 < W:
                 h = height(grid[x2][y2])
-                if (h - curr_height) <= 1 and (x2, y2) not in visited:
+                check_elevation = h - curr_height if go_up else curr_height - h
+                if check_elevation <= 1 and (x2, y2) not in visited:
                     queue.append(path + [(x2, y2)])
                     visited.add((x2, y2))
     return -1
 
-def find_possible_starts(grid):
-    starts = []
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
-            if grid[x][y] == 'a':
-                starts.append((x, y))
-    return starts
-
-def solution_part2(grid):
-    H = len(grid)
-    W = len(grid[0])
-    start, end = find_start_end(grid)
-    starts = find_possible_starts(grid)
-    # Searching back from E
-    start = end
-    for end in starts:
-        visited = set([start])
-        queue = deque([[start]])
-        while queue:
-            path = queue.popleft()
-            x, y = path[-1]
-            # The first 'a' or 'S' found is the closest from E
-            if grid[x][y] == 'a' or grid[x][y] == 'S':
-                return len(path) - 1
-            curr_height = height(grid[x][y])
-            for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-                if 0 <= x2 < H and 0 <= y2 < W:
-                    h = height(grid[x2][y2])
-                    if (curr_height - h) <= 1 and (x2, y2) not in visited:
-                        queue.append(path + [(x2, y2)])
-                        visited.add((x2, y2))
-    return -1
 
 if __name__ == '__main__':
     day = os.path.basename(__file__).split('.')[0]
     grid = aoc_tools.generate_input_filename_and_get_inputs(
         day,
         raw_input=False,
-        test_file=True
+        test_file=False
     )
-    print(f"Solution for {day} part 1 = {solution_part1(grid)}")
-    print(f"Solution for {day} part 2 = {solution_part2(grid)}")
+    start, end = find_start_end(grid)
+    print(f"Solution for {day} part 1 = {find_shortest_path(grid, start, {'E'})}")
+    # Searching back from E => the first 'a' or 'S' found is the closest from E
+    print(f"Solution for {day} part 2 = {find_shortest_path(grid, end, {'S', 'a'}, False)}")
