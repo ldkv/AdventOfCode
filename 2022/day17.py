21f05eb58133ef4ad0d1f7f135b0eb40a79c01b6
Vu LE <ledkvu@gmail.com>
2022-12-17 22:24:38 +0100

    Found solution for day 17 part 2 by guessing the pattern to be a horizontal line which is true for my input.
    The current solution is brute force. Writing new solution to search for repeating pattern.

--- 2022/day17.py
+++ 2022/day17.py
@@ -11,7 +11,7 @@ class Rock:
         [16, 17, 23, 24]        # Square
     ]
     
-    def __init__(self, rock_figure) -> None:
+    def __init__(self, rock_figure):
         self.rock = self.figures[rock_figure]
         self.height = (self.rock[-1] - self.rock[0]) // 7 + 1
     
@@ -33,7 +33,7 @@ class Rock:
             if depth <= 0:
                 rock_level = r // 7
                 floor_level = 3 - rock_level + depth
-                if floor_level < 0 and floor[floor_level][dx] == True:
+                if floor_level < 0 and floor[floor_level][dx]:
                     pushable = False
                     break
 
@@ -47,7 +47,7 @@ class Rock:
             rock_level = r // 7 # 0-3
             next_floor_level = 2 - rock_level + depth
             if next_floor_level < 0:
-                if floor[next_floor_level][r % 7] == True:
+                if floor[next_floor_level][r % 7]:
                     return False
         return True
 
@@ -72,14 +72,14 @@ class Rock:
 class Floor:
     def __init__(self, width):
         self.width = width
-        self.floor = [[True] * self.width]
+        self.floor = [[1] * self.width]
         self.full_floor = [0] * self.width
         self.total_floor = 0
 
     def add_floor(self, depth, rock):
         added_height = 0
         for _ in range(rock.height + depth):
-            new_floor = [False] * self.width
+            new_floor = [0] * self.width
             added_height += 1
             self.floor.append(new_floor)
 
@@ -87,7 +87,7 @@ class Floor:
         for r in rock.rock:
             r_level = r // 7
             floor_level = (3 - r_level) - added_height + depth
-            self.floor[floor_level][r % 7] = True
+            self.floor[floor_level][r % 7] = 1
             self.full_floor[r % 7] = len(self.floor) + floor_level
 
     def cut_floor(self):
@@ -109,11 +109,40 @@ def solution_part1(inputs):
     width = 7
     floor = Floor(width)
     jet_index = 0
-    for r in range(2022):
-        rock = Rock(r % 5)
-        floor, jet_index = rock.drop_rock(jets, jet_index, floor)
-    # render_floor(floor)
-    return len(floor) - 1
+    rock_type = 0
+    for i in range(1, 10000):
+        rock = Rock(rock_type)
+        depth, jet_index = rock.drop_rock(jets, jet_index, floor.floor)
+        floor.add_floor(depth, rock)
+        if sum(floor.floor[-1]) == 7:
+            print(i, floor.total_floor, rock_type, jet_index)
+        rock_type = (rock_type + 1) % 5
+    print(i, floor.total_floor, rock_type, jet_index)
+
+    floor = Floor(width)
+    jet_index = 0
+    rock_type = 0
+    total = 1_000_000_000_000 - 866
+    quotient = total // 1705
+    remain = total - quotient * 1705
+    multi = 1
+    add = 0
+    level_866 = 0
+    level_remain = 0
+    for i in range(1, 866+1706):
+        rock = Rock(rock_type)
+        rock_type = (rock_type + 1) % 5
+        depth, jet_index = rock.drop_rock(jets, jet_index, floor.floor)
+        floor.add_floor(depth, rock)
+        if i == 866:
+            level_866 = floor.total_floor
+        if i == 866 + remain:
+            level_remain = floor.total_floor - level_866
+            print(add)
+    multi = floor.total_floor - level_866
+    add = level_866 + quotient * multi + level_remain
+    print(level_866, level_remain, multi, remain, quotient, add)
+    return floor.total_floor
 
 
 def solution_part2(inputs):
@@ -142,4 +171,4 @@ if __name__ == '__main__':
         test_file=0
     )
     print(f"Solution for {day} part 1 = {solution_part1(inputs)}")
-    print(f"Solution for {day} part 2 = {solution_part2(inputs)}")
+    # print(f"Solution for {day} part 2 = {solution_part2(inputs)}")
